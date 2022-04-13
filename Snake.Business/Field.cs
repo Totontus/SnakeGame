8d38086144c524fa9926faa3141ccb33018d4e89
Raganvald <Raganvald@COMPUTER>
2022-04-13 16:21:08 +0200

    Add project files.

---
+++ Snake.Business/Field.cs
@@ -0,0 +1,83 @@
+﻿namespace Snake.Business
+{
+    public class Field
+    {
+        private readonly List<(int, int)> _snake;
+        private (int, int) _direction;
+        private (int, int) _lastTailPosition = (0, 3);
+        private Random _random;
+
+        public Field(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            _snake = new List<(int, int)> { (0, 2), (0, 1), (0, 0) };
+            _direction = (0, 1);
+            _random = new Random((int)DateTime.Now.Ticks);
+            Spawn_food();
+        }
+
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public bool Next()
+        {
+            (int, int) head = (_snake[0].Item1 + this._direction.Item1, this._snake[0].Item2 + this._direction.Item2);
+            if (head.Item1 >= 0 && head.Item1 < Height && head.Item2 >= 0 && head.Item2 < Width && !this._snake.Contains(head))
+            {
+                this._snake.Insert(0, head);
+                if (head != Food)
+                {
+                    this._lastTailPosition = _snake[^1];
+                    _snake.RemoveAt(this._snake.Count - 1);
+                }
+                else
+                {
+                    Spawn_food();
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public (int, int) Direction
+        {
+            get => this._direction;
+            set
+            {
+                this._direction = value;
+            }
+        }
+
+        public IReadOnlyList<(int, int)> Snake => this._snake;
+
+        public (int, int) Food { get; private set; }
+
+        public (int, int) LastTailPosition => this._lastTailPosition;
+
+        private void Spawn_food()
+        {
+            (int, int)[] freeSquares = GetAllFreeSquares().ToArray();
+            (int, int) _food = freeSquares[_random.Next(0, freeSquares.Length)];
+            Food = _food;
+        }
+
+        private IEnumerable<(int, int)> GetAllFreeSquares()
+        {
+            for (int i = 0; i < Height; ++i)
+            {
+                for (int j = 0; j < Width; ++j)
+                {
+                    if (!this._snake.Contains((i, j)))
+                    {
+                        yield return (i, j);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Snake.ConsoleUI/Program.cs
@@ -0,0 +1,91 @@
+﻿using Snake.Business;
+
+namespace Snake.ConsoleUI
+{
+    public static class Program
+    {
+        private static Dictionary<ConsoleKey, (int, int)> KeyToDirection = new Dictionary<ConsoleKey, (int, int)>()
+    {
+        { ConsoleKey.D, (0, 1) },
+        { ConsoleKey.A, (0, -1) },
+        { ConsoleKey.S, (1, 0) },
+        { ConsoleKey.W, (-1, 0) },
+        { ConsoleKey.RightArrow, (0, 1) },
+        { ConsoleKey.LeftArrow, (0, -1) },
+        { ConsoleKey.DownArrow, (1, 0) },
+        { ConsoleKey.UpArrow, (-1, 0) },
+    };
+
+        public static void Main(string[] args)
+        {
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            Console.CursorVisible = false;
+            Field field = new Field(20, 20);
+            DisplayBorder(field);
+            Thread listener = new Thread(new ParameterizedThreadStart(Listen));
+            listener.Start(field);
+            while (true)
+            {
+                Display(field);
+                Thread.Sleep(100);
+                if (!field.Next())
+                {
+                    return;
+                }
+            }
+
+            Console.SetCursorPosition(0, field.Height + 4);
+            Console.WriteLine("GAME OVER");
+        }
+
+        private static void Listen(object o)
+        {
+            Field field = (Field)o;
+            ConsoleKey key;
+            while (true)
+            {
+                key = Console.ReadKey(true).Key;
+                if (KeyToDirection.TryGetValue(key, out (int, int) direction) && (-direction.Item1, -direction.Item2) != field.Direction)
+                {
+                    field.Direction = direction;
+                }
+            }
+        }
+
+        private static void Display(Field field)
+        {
+            Console.SetCursorPosition(2 + field.LastTailPosition.Item2 * 2, 1 + field.LastTailPosition.Item1);
+            Console.Write("  ");
+
+            foreach (var pair in field.Snake)
+            {
+                Console.SetCursorPosition(2 + pair.Item2 * 2, 1 + pair.Item1);
+                Console.Write("██");
+            }
+
+            Console.SetCursorPosition(2 + field.Food.Item2 * 2, 1 + field.Food.Item1);
+            Console.Write("$$");
+        }
+
+        private static void DisplayBorder(Field field)
+        {
+            string horizontalBorder = new string('#', field.Width * 2 + 4);
+            Console.SetCursorPosition(0, 0);
+            Console.Write(horizontalBorder);
+            Console.SetCursorPosition(0, field.Height + 1);
+            Console.Write(horizontalBorder);
+            for (int i = 0; i < field.Height + 2; ++i)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("##");
+            }
+
+            for (int i = 0; i < field.Height + 2; ++i)
+            {
+                Console.SetCursorPosition(field.Width * 2 + 2, i);
+                Console.Write("##");
+            }
+        }
+    }
+}
